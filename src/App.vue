a49563c38159307c51769aec06fa6951d118cbd3
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-05 03:38:58 +0900

    feat: add routing layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue';
+  import { computed } from 'vue';
+  import { useRoute } from 'vue-router';
+  import DefaultLayout from './layouts/DefaultLayout.vue';
+
+  const route = useRoute();
+
+  const layouts: Record<string, unknown> = {
+    default: DefaultLayout,
+  };
+
+  const currentLayout = computed(() => {
+    const layoutName = (route.meta.layout as string) || 'default';
+    return layouts[layoutName] || layouts.default;
+  });
 </script>
 
 <template>
-  <HelloWorld />
+  <UApp>
+    <component :is="currentLayout">
+      <RouterView />
+    </component>
+  </UApp>
 </template>
