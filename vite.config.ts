095f9c8188b35b3870d4c4d7459d4643fe2e1509
BlueNyang <dksrbxo@bluenyang.kr>
2026-06-04 22:58:54 +0900

    chore: add motion-v and define glassmorhpism theme

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import ui from '@nuxt/ui/vite';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import MotionResolver from 'motion-v/resolver';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), ui()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    ui({
+      components: {
+        dts: true,
+        resolvers: [MotionResolver()],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
